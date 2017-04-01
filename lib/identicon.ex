c0cf7c652b462de12490aade65b4cd5aafb81989
tgisg <soniagupta504@gmail.com>
2017-04-01 11:14:17 -0600

    add hash_input function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,11 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list ##remember using the pipe operator implicitly is like :binary.bin_to_list(:crypto.hash(:md5, input))
   end
 end
