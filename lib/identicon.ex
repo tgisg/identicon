207ec44a9446bd63854e679f093d229c39d0c0f2
tgisg <soniagupta504@gmail.com>
2017-04-01 11:33:44 -0600

    add Identicon.Image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,7 +5,9 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list ##remember using the pipe operator implicitly is like :binary.bin_to_list(:crypto.hash(:md5, input))
+
+    %Identicon.Image{hex: hex}
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+  #sole purpose of this is to hold our struct, which has all the data in our application
+  #we're going to call the list of numbers a hex code. the goal here is to store that list of numbers as this hex property. define struct which has single property called :hex with a default value of nil
+  defstruct hex: nil
+
+end
